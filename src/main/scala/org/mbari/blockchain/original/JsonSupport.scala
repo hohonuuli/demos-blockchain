119861024f79a9259b75469f86bf68f66205b204
Brian Schlining <bschlining@gmail.com>
2017-10-22 21:08:55 -0700

    Added snake case support so it matches original python output

--- src/main/scala/org/mbari/blockchain/original/JsonSupport.scala
+++ src/main/scala/org/mbari/blockchain/original/JsonSupport.scala
@@ -7,7 +7,11 @@ import spray.json.{ DefaultJsonProtocol, JsString, JsValue, JsonFormat }
 
 trait JsonSupport extends SprayJsonSupport {
   // import the default encoders for primitive types (Int, String, Lists etc)
-  import DefaultJsonProtocol._
+  //import DefaultJsonProtocol._
+
+  object SnakifiedSprayJsonSupport extends SnakifiedSprayJsonSupport
+
+  import SnakifiedSprayJsonSupport._
 
   implicit def instantFormat = new JsonFormat[Instant] {
 
@@ -30,4 +34,28 @@ trait JsonSupport extends SprayJsonSupport {
   implicit val nodeResolved1JsonFormat = jsonFormat2(NodeResolved1)
   implicit val nodeResolved2JsonFormat = jsonFormat2(NodeResolved2)
 
+}
+
+/**
+ * A custom version of the Spray DefaultJsonProtocol with a modified field naming strategy
+ */
+trait SnakifiedSprayJsonSupport extends DefaultJsonProtocol {
+  import reflect._
+
+  /**
+   * This is the most important piece of code in this object!
+   * It overrides the default naming scheme used by spray-json and replaces it with a scheme that turns camelcased
+   * names into snakified names (i.e. using underscores as word separators).
+   */
+  override protected def extractFieldNames(classTag: ClassTag[_]) = {
+    import java.util.Locale
+
+    def snakify(name: String) = PASS2.replaceAllIn(PASS1.replaceAllIn(name, REPLACEMENT), REPLACEMENT).toLowerCase(Locale.US)
+
+    super.extractFieldNames(classTag).map { snakify(_) }
+  }
+
+  private val PASS1 = """([A-Z]+)([A-Z][a-z])""".r
+  private val PASS2 = """([a-z\d])([A-Z])""".r
+  private val REPLACEMENT = "$1_$2"
 }
